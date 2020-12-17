ae2fd9f239f8878b0cd143977264402a85f207ea
4423 <hanamiya4423@gmail.com>
2020-12-17 14:31:53 +0900

    fix: bench

--- benchmarks/fix_functor/fix_functor.cod.ml
+++ benchmarks/fix_functor/fix_functor.cod.ml
@@ -31,15 +31,16 @@ let suppressAddZeroOrMulZeroPE = fun (m: (module S with type obs_t = int) mcod) 
       let int = fun n1 -> if n1 = 0 then (.~($m.int) 0, true) else (.~($m.int) n1, false)
       let add = fun n1 -> fun n2 -> 
         match (n1, n2) with
-          (n1, b1), (n2, b2) -> if (b1 && b2) then (.~($m.int) 0, true) else .~($m.add) n1 n2
+          (n1, b1), (n2, b2) -> if (b1 && b2) then (.~($m.int) 0, true) else (.~($m.add) n1 n2, false)
       let sub = fun n1 -> fun n2 ->
-        if n1 = n2 then (.~($m.int) 0, true) else .~($m.sub) n1 n2
+        match (n1, n2) with
+          (n1, _), (n2, _) -> if n1 = n2 then (.~($m.int) 0, true) else (.~($m.sub) n1 n2, false)
       let mul = fun n1 -> fun n2 ->
         match (n1, n2) with
-          (n1, b1), (n2, b2) -> if (b1 || b2) then (.~($m.int) 0, true) else .~($m.mul) n1 n2
+          (n1, b1), (n2, b2) -> if (b1 || b2) then (.~($m.int) 0, true) else (.~($m.mul) n1 n2, false)
       let div = fun n1 -> fun n2 ->
         match (n1, n2) with
-          (n1, _), (n2, _) -> .~($m.div) n1 n2, false
+          (n1, _), (n2, _) -> (.~($m.div) n1 n2, false)
       let observe = fun f ->
         match f 0 with
           (n, _) -> .~($m.observe) (fun _ -> n)
--- benchmarks/set_functor/set_functor.cod.ml
+++ benchmarks/set_functor/set_functor.cod.ml
@@ -9,8 +9,8 @@ module type S = sig
   val member: elt -> set -> bool
 end
 
-let makeSet (type a) = fun (m: (module EQ with type t = a) code) ->
-  .<(module struct
+let makeSet (type a) = fun (m: (module EQ with type t = a) mcod) ->
+  .<<(module struct
       type elt = $m.t
       type set = $m.t list
 
@@ -19,12 +19,12 @@ let makeSet (type a) = fun (m: (module EQ with type t = a) code) ->
           hd :: tl -> .~($m.eq) elt hd || member elt tl
         | _        -> false
 
-     end: S with type elt = a and type set = a list)>.
+     end: S with type elt = a and type set = a list)>>.
 ;;
-let intset_cod = makeSet .<(module struct
+let intset_cod = makeSet .<<(module struct
     type t = int
     let eq = fun x -> fun y -> x = y
-end : EQ with type t = int)>.
+end : EQ with type t = int)>>.
 ;;
 let intset = (module struct
   type elt = $intset_cod.elt
