5c33b7a7cb1467c351de0fe9fb1e7c5e29fc68e2
4423 <hanamiya4423@gmail.com>
2021-03-04 17:28:48 +0900

    update: examples

--- example/fix_functor.ml
+++ example/fix_functor.ml
@@ -1,44 +1,54 @@
 module type S = sig
   type int_t
+  type obs_t
+  type unit_t
   val int: int -> int_t
   val add: int_t -> int_t -> int_t
   val sub: int_t -> int_t -> int_t
   val mul: int_t -> int_t -> int_t
   val div: int_t -> int_t -> int_t
+  val observe: (unit_t -> int_t) -> obs_t
 end
 
-let arith = .<(module struct
+let arith = .<<(module struct
   type int_t = int
+  type obs_t = int
+  type unit_t = int
   let int = fun n1 -> n1
   let add = fun n1 -> fun n2 -> n1 + n2
   let sub = fun n1 -> fun n2 -> n1 - n2
   let mul = fun n1 -> fun n2 -> n1 * n2
   let div = fun n1 -> fun n2 -> n1 / n2
-end: S)>.
+  let observe = fun f -> f 0
+end: S with type obs_t = int)>>.
 ;;
 
-let suppressAddZeroOrMulZeroPE = fun (m: (module S) code) ->
-  .<(module struct
+let suppressAddZeroOrMulZeroPE = fun (m: (module S with type obs_t = int) mcod) ->
+  .<<(module struct
       type int_t = $m.int_t * bool
+      type obs_t = int
+      type unit_t = int
       let int = fun n1 -> if n1 = 0 then (.~($m.int) 0, true) else (.~($m.int) n1, false)
       let add = fun n1 -> fun n2 -> 
         match (n1, n2) with
-          (n1, b1), (n2, b2) -> if (b1 && b2) then (.~($m.int) 0, true) else .~($m.add) n1 n2
+          (n1, b1), (n2, b2) -> if (b1 && b2) then (.~($m.int) 0, true) else (.~($m.add) n1 n2, false)
       let sub = fun n1 -> fun n2 ->
-        if n1 = n2 then (.~($m.int) 0, true) else .~($m.sub) n1 n2
+        match (n1, n2) with
+          (n1, _), (n2, _) -> if n1 = n2 then (.~($m.int) 0, true) else (.~($m.sub) n1 n2, false)
       let mul = fun n1 -> fun n2 ->
         match (n1, n2) with
-          (n1, b1), (n2, b2) -> if (b1 || b2) then (.~($m.int) 0, true) else .~($m.mul) n1 n2
+          (n1, b1), (n2, b2) -> if (b1 || b2) then (.~($m.int) 0, true) else (.~($m.mul) n1 n2, false)
       let div = fun n1 -> fun n2 ->
         match (n1, n2) with
-          (n1, _), (n2, _) -> .~($m.div) n1 n2, false
-    end: S)>.
+          (n1, _), (n2, _) -> (.~($m.div) n1 n2, false)
+      let observe = fun f ->
+        match f 0 with
+          (n, _) -> .~($m.observe) (fun _ -> n)
+    end: S with type obs_t = int)>>.
 ;;
 let rec fix depth m =
   if depth <= 0 then
     m
   else
     fix (depth - 1) (suppressAddZeroOrMulZeroPE m)
-;;
-let fix = fix 1000 arith
 ;;
--- example/set_functor.ml
+++ example/set_functor.ml
@@ -9,8 +9,8 @@ module type S = sig
   val member: elt -> set -> bool
 end
 
-let makeSet (type a) = fun (m: (module EQ with type t = a) code) ->
-  .<(module struct
+let makeSet (type a) = fun (m: (module EQ with type t = a) mcod) ->
+  .<<(module struct
       type elt = $m.t
       type set = $m.t list
 
@@ -19,16 +19,12 @@ let makeSet (type a) = fun (m: (module EQ with type t = a) code) ->
           hd :: tl -> .~($m.eq) elt hd || member elt tl
         | _        -> false
 
-     end: S with type elt = a and type set = a list)>.
+     end: S with type elt = a and type set = a list)>>.
 ;;
-let intset_cod = makeSet .<(module struct
+let intset_cod = makeSet .<<(module struct
     type t = int
     let eq = fun x -> fun y -> x = y
-end : EQ with type t = int)>.
+end : EQ with type t = int)>>.
 ;;
-let intset = (module struct
-  type elt = $intset_cod.elt
-  type set = $intset_cod.set
-  let member = Runcode.run ($intset_cod.member)
-end: S with type elt = int and type set = int list)
+let intset = (run_module intset_cod: S with type elt = int and type set = int list)
 ;;
